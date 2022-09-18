92fe93bacd10aadcb4a3cc8e7406bb4b2b634005
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2022-09-18 19:37:13 +0200

    Add some files v2

--- front/NBAAPP/src/app/app-routing.module.ts
+++ front/NBAAPP/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { PlayersComponent } from './components/players/players.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : "menu", component:MainMenuComponent},
+  { path : "players", component:PlayersComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/NBAAPP/src/app/app.module.ts
+++ front/NBAAPP/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { PlayersComponent } from './components/players/players.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    PlayersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
